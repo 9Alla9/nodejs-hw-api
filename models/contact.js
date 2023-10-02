a8d62a4fed91a535450f2bf94d09861eee12f66f
Alla <119464974+9Alla9@users.noreply.github.com>
2023-10-02 09:08:23 +0300

    Revert "Revert "завдання 5  ""

--- models/contact.js
+++ models/contact.js
@@ -1,34 +1,34 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
-  },
-  email: {
-    type: String,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-  owner: {
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
       type: Schema.Types.ObjectId,
       ref: "user",
       required: true,
-  }
-},
-// щоб уникнути запису в БД версії але не часу
-{ versionKey: false, timestamps: true }
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
 const addSchema = Joi.object({
--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 require("dotenv").config();
-
 const { DB_HOST, PORT } = process.env;
 
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true); // mongoose
 
 mongoose
   .connect(DB_HOST)
